a4105565cfd7b43fabfe79163cd6878d01ca06b2
Sonaksh Saraswat <sonaksh.cse15@nituk.ac.in>
2023-03-26 14:49:55 +0530

    fix dir structure

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Head from "next/head";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import OurPeople from "../components/OurPeople";
-import About from "../components/About";
-import NextSteps from "../components/NextSteps";
+import Header from "../src/components/Header";
+import Hero from "../src/components/Hero";
+import OurPeople from "../src/components/OurPeople";
+import About from "../src/components/About";
+import NextSteps from "../src/components/NextSteps";
 import Ministries from "@/components/Ministries";
-import Events from "../components/Events";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
-import "./globals.css";
+import Events from "../src/components/Events";
+import Contact from "../src/components/Contact";
+import Footer from "../src/components/Footer";
+import "../src/app/globals.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 export default function Home() {
